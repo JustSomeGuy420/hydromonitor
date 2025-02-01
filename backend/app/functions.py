4d52de6f425eb0854bdeeeb8864b405af2ab891f
Alwyn Sterling <alwynsterling5@gmail.com>
2025-02-01 11:38:28 -0500

    Finished Lab 3

--- backend/app/functions.py
+++ backend/app/functions.py
@@ -71,8 +71,8 @@ class DB:
         try:
             remotedb 	= self.remoteMongo('mongodb://%s:%s@%s:%s' % (self.username, self.password,self.server,self.port), tls=self.tls)
             result      = list(remotedb.ELET2415.climo.find({"timestamp":{"$gte":int(start),"$lte":int(end)}}, {"_id":0}).sort("timestamp",1))
-            print(start)
-            print(end)
+            #print(result)
+            #print(end)
         except Exception as e:
             msg = str(e)
             print("getAllInRange error ",msg)            
@@ -107,7 +107,7 @@ class DB:
         '''RETURNS THE FREQUENCY DISTROBUTION FOR A SPECIFIED VARIABLE WITHIN THE START AND END DATE RANGE'''
         try:
             remotedb 	= self.remoteMongo('mongodb://%s:%s@%s:%s' % (self.username, self.password,self.server,self.port), tls=self.tls)
-            result      = list(remotedb.ELET2415.climo.aggregate([{"$match": {"timestamp": {"$gte": int(start), "$lte": int(end)}}}, {"$bucket": {"groupBy": variable, "boundaries": [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], "default": "outliers", "output": {"count": {"$sum": 1}}}}]))
+            result      = list(remotedb.ELET2415.climo.aggregate([{"$match": {"timestamp": {"$gte": int(start), "$lte": int(end)}}}, {"$bucket": {"groupBy": "$" + variable, "boundaries": [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], "default": "outliers", "output": {"count": {"$sum": 1}}}}]))
         except Exception as e:
             msg = str(e)
             print("frequencyDistro error ",msg)            
